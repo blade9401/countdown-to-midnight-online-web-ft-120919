1f0353e8782c1a5ff65fff0dcf40c9178c46af98
Eric murray <ericwmurray1994@gmail.com>
2019-12-19 05:27:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+number = 10
+def countdown(number)
+  while number > 0 
+    puts "#{number} Second(s)!".upcase
+    number -= 1
+  end
+  "happy new year!".upcase
+end
 
-def countdown
+def countdown_with_sleep("countdown")
+  sleep(5)
 end
